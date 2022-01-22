c3265f147837a5f560cab6f360d5d6635570b4d2
TheNoFace <thenoface303@gmail.com>
2022-01-22 11:08:09 +0900

    idle-assist.sh: Initial commit

---
+++ idle-assist.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+#------------------------------------------------------------------
+#
+# Now Downloader
+# Created on 2022 Jan 21
+# Author: TheNoFace (thenoface303@gmail.com)
+# Version 1.0.0
+#
+#------------------------------------------------------------------
+
+[[ $(id -u) != 0 ]] && echo "Do sudo!" && exit 255
+
+if [ -z $1 ]
+then
+	refreshInt=60
+else
+	refreshInt=$1
+fi
+
+function msg()
+{
+	echo -e "[$(date +'%F %T')] $1"
+}
+
+function is_active()
+{
+	sdcStatus=$(hdparm -C /dev/sdc | awk 'FNR == 3 {print $4}')
+	sddStatus=$(hdparm -C /dev/sdd | awk 'FNR == 3 {print $4}')
+	msg "hdparm sdc: ${sdcStatus} / sdd: ${sddStatus}"
+
+	if [[ ${sdcStatus} != 'standby' ]] || [[ ${sddStatus} != 'standby' ]]
+	then
+		isActive=1
+	else
+		unset isActive
+	fi
+}
+
+function io_check()
+{
+	sdcLog=$(tac /var/log/syslog | grep "sdc" -m 1)
+	sddLog=$(tac /var/log/syslog | grep "sdd" -m 1)
+
+	sdcLength=$(echo ${sdcLog} | wc -m)
+	sddLength=$(echo ${sddLog} | wc -m)
+
+	# hd-idle init: 252 | spinup/down: 55/53 | default: about 220
+	if [[ ${sdcLength} -lt 100 ]] || [[ ${sdcLength} -gt 240 ]] || [[ ${sddLength} -lt 100 ]] || [[ ${sddLength} -gt 240 ]]
+	then
+		sleep 60
+		io_check
+	fi
+
+	sdcRead=$(echo "${sdcLog}" | grep -oP 'reads=\K[^ ]+')
+	sdcWrite=$(echo "${sdcLog}" | grep -oP 'writes=\K[^ ]+')
+	sddRead=$(echo "${sddLog}" | grep -oP 'reads=\K[^ ]+')
+	sddWrite=$(echo "${sddLog}" | grep -oP 'writes=\K[^ ]+')
+}
+
+function spin_check()
+{
+	sdcLog=$(tac /var/log/syslog | grep "sdc" -m 1)
+	sddLog=$(tac /var/log/syslog | grep "sdd" -m 1)
+
+	sdcLength=$(echo ${sdcLog} | wc -m)
+	sddLength=$(echo ${sddLog} | wc -m)
+
+	# hd-idle init: 252 | spinup/down: 55/53 | default: about 220
+	if [[ ${sdcLength} -lt 100 ]] || [[ ${sdcLength} -gt 240 ]] || [[ ${sddLength} -lt 100 ]] || [[ ${sddLength} -gt 240 ]]
+	then
+		msg "sdcLength: ${sdcLength} / sddLength: ${sddLength}"
+		msg "Wrong log, re-check in 60 seconds"
+		sleep 60
+		spin_check
+	fi
+
+	sdcSpinDown=$(tac /var/log/syslog | grep "sdc" -m 1 | grep -oP 'spunDown=\K[^ ]+')
+	sddSpinDown=$(tac /var/log/syslog | grep "sdd" -m 1 | grep -oP 'spunDown=\K[^ ]+')
+	msg "hd-idle spundown sdc: ${sdcSpinDown} / sdd: ${sddSpinDown}"
+}
+
+main()
+{
+	is_active
+	if [[ ! -z ${isActive} ]]
+	then
+		msg "Found active disk!"
+		spin_check
+		if [[ ${sdcSpinDown} == 'false' ]] || [[ ${sddSpinDown} == 'false' ]]
+		then
+			msg "hd-idle is still waiting for disk(s) to spindown, re-check in 60 seconds"
+			sleep 60
+			main
+		fi
+
+		if [[ ${sdcStatus} != 'standby' ]] || [[ ${sddStatus} != 'standby' ]]
+		then
+			msg "Spin down disk(s) in 5 minutes"
+			sleep 300
+		fi
+
+		if [[ ${sdcStatus} != 'standby' ]] && [[ ${sdcSpinDown} == 'true' ]]
+		then
+			msg "Spinning down sdc..."
+			hd-idle -t /dev/sdc
+		fi
+
+		if [[ ${sddStatus} != 'standby' ]] && [[ ${sddSpinDown} == 'true' ]]
+		then
+			msg "Spinning down sdd..."
+			hd-idle -t /dev/sdd
+		fi
+
+		main # loop
+		exit 11 # wrong exit
+	elif [[ -z ${isActive} ]]
+	then
+		sleep 60
+		main # loop
+		exit 12 #wrong exit
+	else
+		msg "ERROR: Unknown disk state sdc: ${sdcStatus} / sdd: ${sddStatus}"
+		exit 1
+	fi
+}
+
+msg "Refresing every ${refreshInt}s"
+main
+msg "ERROR: EOF"
+exit 10
